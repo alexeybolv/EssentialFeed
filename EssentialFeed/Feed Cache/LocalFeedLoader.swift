f77db67f18f51a6215fca567ed90a1826621cb05
Alexey Bolvonovich <alexeybolv@gmail.com>
2024-01-19 04:22:56 +0100

    Load command delivers no images on empty cache

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -35,6 +35,8 @@ final public class LocalFeedLoader {
         store.retrieve { error  in
             if let error = error {
                 completion(.failure(error))
+            } else {
+                completion(.success([]))
             }
         }
     }
