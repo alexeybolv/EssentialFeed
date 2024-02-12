0e1e2697291089b8216b11a714a4b18a31f25a78
Alexey Bolvonovich <alexeybolv@gmail.com>
2024-02-12 19:33:00 +0100

    Add EssentialFeedCacheintegrationTests' target to separate the potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,11 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Alexey Bolvonovich on 12/02/2024.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+}
