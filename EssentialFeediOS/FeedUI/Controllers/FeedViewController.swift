0d6469819738bbb207697065e1e41d2b2aad8770
Alexey Bolvonovich <alexeybolv@gmail.com>
2024-04-03 16:36:00 +0200

    Remove mutable state from FeedViewModel. The 'FeedViewModel only needs to forward state changes, so state is only transient.

--- EssentialFeediOS/FeedUI/Controllers/FeedViewController.swift
+++ EssentialFeediOS/FeedUI/Controllers/FeedViewController.swift
@@ -28,8 +28,8 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
         tableView.prefetchDataSource = self
         
         onViewIsAppearing = { vc in
-            vc.refreshController?.refresh()
             vc.onViewIsAppearing = nil
+            vc.refreshController?.refresh()
         }
     }
     
