9ca1d56be98f09ef33c00cc60ad94f6f60f58cf3
Alexey Bolvonovich <alexeybolv@gmail.com>
2024-04-10 17:04:42 +0200

    Move ViewModels to separate files

---
+++ EssentialFeediOS/FeedPresentation/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Alexey Bolvonovich on 10/04/2024.
+//
+
+import Foundation
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
