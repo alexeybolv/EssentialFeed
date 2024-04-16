4d70f36de9e0b488a321c39702363aee476ca8c3
Alexey Bolvonovich <alexeybolv@gmail.com>
2024-04-16 19:55:48 +0200

    Set "FeedViewController" title

--- EssentialFeediOS/FeedUI/Controllers/FeedViewController.swift
+++ EssentialFeediOS/FeedUI/Controllers/FeedViewController.swift
@@ -22,6 +22,7 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
     public override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "My Feed"
         tableView.prefetchDataSource = self
         
         onViewIsAppearing = { [weak self] vc in
