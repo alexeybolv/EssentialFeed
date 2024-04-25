71045140dada304a7ba45848ad5f1e71b2548e8a
Alexey Bolvonovich <alexeybolv@gmail.com>
2024-04-25 22:30:00 +0200

    Move FeedErrorViewModel to separate file

---
+++ EssentialFeediOS/FeedPresentation/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Alexey Bolvonovich on 25/04/2024.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
