5122a15e06122bd3999f7201d703c4ed994e94ae
Alexey Bolvonovich <alexeybolv@gmail.com>
2024-05-14 23:53:30 +0200

    Extract FeedImageDataLoader test helpers into a shared protocol extension to remove duplication

--- EssentialApp/EssentialAppTests/Helpers/XCTestCase+FeedImageDataLoader.swift
+++ EssentialApp/EssentialAppTests/Helpers/XCTestCase+FeedImageDataLoader.swift
@@ -1,5 +1,5 @@
 //
-//  XCTestCase+FeedLoader.swift
+//  XCTestCase+FeedImageDataLoader.swift
 //  EssentialAppTests
 //
 //  Created by Alexey Bolvonovich on 14/05/2024.
@@ -8,25 +8,29 @@
 import XCTest
 import EssentialFeed
 
-protocol FeedLoaderTestCase: XCTestCase {}
+protocol FeedImageDataLoaderTestCase: XCTestCase {}
 
-extension FeedLoaderTestCase {
-    func expect(_ sut: FeedLoader, toCompleteWith expectedResult: FeedLoader.Result, file: StaticString = #file, line: UInt = #line) {
-        let exp = expectation (description: "Wait for load completion" )
-        
-        sut.load { receivedResult in
+extension FeedImageDataLoaderTestCase {
+    func expect(_ sut: FeedImageDataLoader, toCompleteWith expectedResult: FeedImageDataLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
+        let exp = expectation(description: "Wait for load completion")
+
+        _ = sut.loadImageData(from: anyURL()) { receivedResult in
             switch (receivedResult, expectedResult) {
             case let (.success(receivedFeed), .success(expectedFeed)):
                 XCTAssertEqual(receivedFeed, expectedFeed, file: file, line: line)
+
             case (.failure, .failure):
                 break
+
             default:
                 XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
             }
-            
+
             exp.fulfill()
         }
-        
+
+        action()
+
         wait(for: [exp], timeout: 1.0)
     }
 }
