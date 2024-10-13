6fadc9b5bc756615b8a8b74508518950687ff26a
Alexey Bolvonovich <alexeybolv@gmail.com>
2024-10-13 02:57:42 +0200

    Test FeeditemsMapper in isolation

---
+++ EssentialFeedTests/Feed API/FeedItemsMapperTests.swift
@@ -0,0 +1,84 @@
+//
+//  FeedItemsMapperTests.swift
+//  EssentialFeedTests
+//
+//  Created by Alexey Bolvonovich on 11.08.23.
+//
+
+import XCTest
+import EssentialFeed
+
+final class FeedItemsMapperTests: XCTestCase {
+    
+    func test_map_throwsErrorOnNon200HTTPResponse() throws {
+        let json = makeItemsJSON([])
+        let samples = [199, 201, 300, 400, 500]
+        
+        try samples.forEach { code in
+            XCTAssertThrowsError(
+                try FeedItemsMapper.map(json, from: HTTPURLResponse(statusCode: code))
+            )
+        }
+    }
+    
+    func test_map_throwsErrorOn200HTTPResponseWithInvalidJSON() {
+        let invalidData = Data("Data".utf8)
+        
+        XCTAssertThrowsError(
+            try FeedItemsMapper.map(invalidData, from: HTTPURLResponse(statusCode: 200))
+        )
+    }
+    
+    func test_map_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() throws {
+        let emptyListJSON = makeItemsJSON([])
+        
+        let result = try FeedItemsMapper.map(emptyListJSON, from: HTTPURLResponse(statusCode: 200))
+        
+        XCTAssertEqual(result, [])
+    }
+    
+    func test_map_deliversItemsOn200HTTPResponseWithJSONItems() throws {
+        let item1 = makeItem(
+            id: UUID(),
+            imageURL: URL(string: "www.youtube.com")!
+        )
+        let item2 = makeItem(
+            id: UUID(),
+            description: "description",
+            location: "location",
+            imageURL: URL(string: "www.youtube.com")!
+        )
+        let items = [item1.model, item2.model]
+        let jsonData = makeItemsJSON([item1.json, item2.json])
+        
+        let result = try FeedItemsMapper.map(jsonData, from: HTTPURLResponse(statusCode: 200))
+        
+        XCTAssertEqual(result, items)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeItem(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) -> (model: FeedImage, json: [String: Any]) {
+        let item = FeedImage(id: id, description: description, location: location, url: imageURL)
+        
+        let json = [
+            "id": item.id.uuidString,
+            "description": item.description,
+            "location": item.location,
+            "image": item.url.absoluteString
+        ]
+        
+        return (model: item, json: json as [String : Any])
+    }
+    
+    private func makeItemsJSON(_ items: [[String: Any]]) -> Data {
+        let json = ["items": items]
+        return try! JSONSerialization.data(withJSONObject: json)
+    }
+}
+
+private extension HTTPURLResponse {
+    convenience init(statusCode: Int) {
+        self.init(url: anyURL(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
+    }
+}
